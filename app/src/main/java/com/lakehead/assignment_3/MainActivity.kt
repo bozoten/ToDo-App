f042e175085ecf1a103ce682724e4e956deea9d7
Pavelrh <shridharapavel@gmail.com>
2024-03-24 22:51:55 -0400

    added internal documentation

--- app/src/main/java/com/lakehead/assignment_3/MainActivity.kt
+++ app/src/main/java/com/lakehead/assignment_3/MainActivity.kt
@@ -3,6 +3,15 @@ package com.lakehead.assignment_3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Filename: MainActivity.kt
+ * Project: Assignment 3
+ * Author's name: Shridhara Pavel Rahul Uma
+ * Student Id: 1175516
+ * Date: 24-03-2024
+ * Description: This is a ui design for a simple to do app
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
